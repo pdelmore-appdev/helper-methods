39aee693785f958f70b805c79c8acb931def73e6
Paulina Delmore <delmore.paulina@gmail.com>
2023-03-31 19:02:30 +0000

    add resources :movies to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,32 +1,36 @@
 Rails.application.routes.draw do
   root "movies#index"
 
-  # Routes for the Movie resource:
 
-  # CREATE
-  post "/movies" => "movies#create", as: :movies 
-    # gives movies_url and movies_path
+  resources :movies
 
 
-  get "/movies/new" => "movies#new", as: :new_movie
-    # gives new_movie_url and new_movie_path
+  # # Routes for the Movie resource:
+
+  # # CREATE
+  # post "/movies" => "movies#create", as: :movies 
+  #   # gives movies_url and movies_path
+
+
+  # get "/movies/new" => "movies#new", as: :new_movie
+  #   # gives new_movie_url and new_movie_path
           
-  # READ
-  get "/movies" => "movies#index"
+  # # READ
+  # get "/movies" => "movies#index"
 
   
   
-  get "/movies/:id" => "movies#show", as: :movie
-  # gives movie_path() -> will expect argument for id
+  # get "/movies/:id" => "movies#show", as: :movie
+  # # gives movie_path() -> will expect argument for id
 
   
-  # UPDATE
-  patch "/movies/:id" => "movies#update"
-  get "/movies/:id/edit" => "movies#edit", as: :edit_movie
-    # gives edit_movie_path() -> will expect argument for id
+  # # UPDATE
+  # patch "/movies/:id" => "movies#update"
+  # get "/movies/:id/edit" => "movies#edit", as: :edit_movie
+  #   # gives edit_movie_path() -> will expect argument for id
   
-  # DELETE
-  delete "/movies/:id" => "movies#destroy"
+  # # DELETE
+  # delete "/movies/:id" => "movies#destroy"
 
   #------------------------------
 end
