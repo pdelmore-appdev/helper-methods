756467f7f29e8c626ed176237d39d342c5ec4182
Paulina Delmore <delmore.paulina@gmail.com>
2023-03-31 15:08:12 +0000

    give names to routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,16 +4,26 @@ Rails.application.routes.draw do
   # Routes for the Movie resource:
 
   # CREATE
-  post "/movies" => "movies#create"
-  get "/movies/new" => "movies#new"
+  post "/movies" => "movies#create", as: :movies 
+    # gives movies_url and movies_path
+
+
+  get "/movies/new" => "movies#new", as: :new_movie
+    # gives new_movie_url and new_movie_path
           
   # READ
   get "/movies" => "movies#index"
-  get "/movies/:id" => "movies#show"
+
+  
+  
+  get "/movies/:id" => "movies#show", as: :movie
+  # gives movie_path() -> will expect argument for id
+
   
   # UPDATE
   patch "/movies/:id" => "movies#update"
-  get "/movies/:id/edit" => "movies#edit"
+  get "/movies/:id/edit" => "movies#edit", as: :edit_moive
+    # gives edit_movie_path() -> will expect argument for id
   
   # DELETE
   delete "/movies/:id" => "movies#destroy"
